060499e0f5ae392d117f14e7e7a12f1867dd0d52
SYED UMAR <umar630934@gmail.com>
2024-07-28 23:57:49 +0530

    fix: Made some responsive changes

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,108 +1,192 @@
-import React, { useState } from 'react';
-import './UserForm.css';
-import userimage from '../assets/user-image.png';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import "./UserForm.css";
+import userimage from "../assets/user-image.png";
 
 const UserForm = () => {
-    const [isYesChecked, setIsYesChecked] = useState(false);
-    const [isNoChecked, setIsNoChecked] = useState(false);
-    const [isSubFormYesChecked, setIsSubFormYesChecked] = useState(false);
-    const [isSubFormNoChecked, setIsSubFormNoChecked] = useState(false);
+  const [isYesChecked, setIsYesChecked] = useState(false);
+  const [isNoChecked, setIsNoChecked] = useState(false);
+  const [isSubFormYesChecked, setIsSubFormYesChecked] = useState(false);
+  const [isSubFormNoChecked, setIsSubFormNoChecked] = useState(false);
+  const [attendees, setAttendees] = useState([{ name: "", relation: "", file: null }]);
 
-    const handleYesChange = (event) => {
-        setIsYesChecked(event.target.checked);
-        if (event.target.checked) {
-            setIsNoChecked(false);
-        }
-    };
+  const handleYesChange = (event) => {
+    setIsYesChecked(event.target.checked);
+    if (event.target.checked) {
+      setIsNoChecked(false);
+    }
+  };
 
-    const handleNoChange = (event) => {
-        setIsNoChecked(event.target.checked);
-        if (event.target.checked) {
-            setIsYesChecked(false);
-        }
-    };
+  const handleNoChange = (event) => {
+    setIsNoChecked(event.target.checked);
+    if (event.target.checked) {
+      setIsYesChecked(false);
+      setIsSubFormYesChecked(false);
+    }
+  };
 
-    const handleSubFormYesChange = (event) => {
-        setIsSubFormYesChecked(event.target.checked);
-        if (event.target.checked) {
-            setIsSubFormNoChecked(false);
-        }
-    };
+  const handleSubFormYesChange = (event) => {
+    setIsSubFormYesChecked(event.target.checked);
+    if (event.target.checked) {
+      setIsSubFormNoChecked(false);
+    }
+  };
 
-    const handleSubFormNoChange = (event) => {
-        setIsSubFormNoChecked(event.target.checked);
-        if (event.target.checked) {
-            setIsSubFormYesChecked(false);
-        }
-    };
+  const handleSubFormNoChange = (event) => {
+    setIsSubFormNoChecked(event.target.checked);
+    if (event.target.checked) {
+      setIsSubFormYesChecked(false);
+    }
+  };
 
-    return (
-        <>
+  const handleAddAttendee = () => {
+    setAttendees([...attendees, { name: "", relation: "", file: null }]);
+  };
+
+  const handleRemoveAttendee = (index) => {
+    setAttendees(attendees.filter((_, i) => i !== index));
+  };
+
+  const handleAttendeeChange = (index, field, value) => {
+    const newAttendees = attendees.map((attendee, i) =>
+      i === index ? { ...attendee, [field]: value } : attendee
+    );
+    setAttendees(newAttendees);
+  };
+
+  const handleFileChange = (index, file) => {
+    const newAttendees = attendees.map((attendee, i) =>
+      i === index ? { ...attendee, file } : attendee
+    );
+    setAttendees(newAttendees);
+  };
+
+  return (
+    <div className="full-container">
+      <div className="semi-full-container">
         <div className="title">
-                <h4>Student Registration Form</h4>
+          <h4>Student Registration Form</h4>
         </div>
         <div className="main-container">
+          <div className="container">
+            <div className="descp">
+              <p>Are you willing to attend?</p>
+            </div>
+            <div className="checkboxes">
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isYesChecked}
+                  onChange={handleYesChange}
+                  id="cb-yes"
+                />
+                Yes
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isNoChecked}
+                  onChange={handleNoChange}
+                  id="cb-no"
+                />
+                No
+              </label>
+            </div>
 
-            <div className='container'>
-        
+            {isYesChecked && (
+              <form className="form-style">
+                <div className="column-container">
+                  <img
+                    src={userimage}
+                    alt="User"
+                    style={{ width: "75px", height: "75px" }}
+                  />
+                </div>
+                <div className="column-container">
+                  <b>Name</b>
+                </div>
+                <div className="column-container">
+                  <p>Hall Ticket Number</p>
+                </div>
                 <div className="descp">
-                    <label>Are you willing to Attend ?</label>
+                  <label>Persons additionally attending with you?{" "}</label>
                 </div>
                 <div className="checkboxes">
-                    <label><input type="checkbox" checked={isYesChecked} onChange={handleYesChange} id="cb"/>Yes</label>
-                    <label><input type="checkbox" checked={isNoChecked} onChange={handleNoChange} id="cb"/>No</label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={isSubFormYesChecked}
+                      onChange={handleSubFormYesChange}
+                      id="subform-yes"
+                    />
+                    Yes
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={isSubFormNoChecked}
+                      onChange={handleSubFormNoChange}
+                      id="subform-no"
+                    />
+                    No
+                  </label>
                 </div>
+              </form>
+            )}
 
-                {isYesChecked && (
-                    <form action="">
-                        <div class="input-container">
-                            <input type="text" placeholder="Hall Ticket No"/>
-                        </div>
-                        <div className="column-container">
-                            <b>Name</b>
-                        </div>
-                        <div className="column-container">
-                            <img src={userimage} alt="" style={{ width: '75px', height: '75px' }} />
-                        </div>
-                        <div className="descp">
-                            <label>Persons accompanying for the graduation fill the form :</label>
-                        </div>
-                        <div className="checkboxes">
-                            <label><input type="checkbox" checked={isSubFormYesChecked} onChange={handleSubFormYesChange} id="cb"/>Yes</label>
-                            <label><input type="checkbox" checked={isSubFormNoChecked} onChange={handleSubFormNoChange} id="cb"/>No</label>
-                        </div>
-                    </form>
-                )}
-                
-                {isSubFormYesChecked && (
-                    <form action="">
-                    <div className="subform">
-                        <div className="row-container">
-
-                            <div className="sub-container">
-                                <input type="text" placeholder='Name'/>
-                                <input type="text" placeholder='Relation'/>
-                                <input type="file" />
-                            </div>
-
-                            <div className="sub-container">
-                                <input type="text" placeholder='Name'/>
-                                <input type="text" placeholder='Relation'/>
-                                <input type="file" />
-                            </div>
-
-                            <div className="submit-btn">
-                                <button type='submit'>Submit</button>
-                            </div>
-                        </div>
+            {isSubFormYesChecked && (
+              <form action="">
+                <div className="subform">
+                  {attendees.map((attendee, index) => (
+                    <div className="sub-container" key={index}>
+                      <button
+                        type="button"
+                        className="remove-btn"
+                        onClick={() => handleRemoveAttendee(index)}
+                      >
+                        Remove
+                      </button>
+                      <input
+                        type="text"
+                        placeholder="Name"
+                        value={attendee.name}
+                        onChange={(e) =>
+                          handleAttendeeChange(index, "name", e.target.value)
+                        }
+                      />
+                      <input
+                        type="text"
+                        placeholder="Relation"
+                        value={attendee.relation}
+                        onChange={(e) =>
+                          handleAttendeeChange(index, "relation", e.target.value)
+                        }
+                      />
+                      <input
+                      placeholder="Upload Image"
+                        type="file"
+                        onChange={(e) =>
+                          handleFileChange(index, e.target.files[0])
+                        }
+                      />
                     </div>
-                    </form>
-                )}
-
-            </div>
+                  ))}
+                  <div className="add-more-btn">
+                    <button type="button" onClick={handleAddAttendee}>
+                      Add More
+                    </button>
+                  </div>
+                  <div className="submit-btn">
+                    <button type="submit">Submit</button>
+                  </div>
+                </div>
+              </form>
+            )}
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserForm;
